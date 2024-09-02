03fb3c145d7b7f76596d200661dcb6ff4dc714fa
Zhang Elisa <78491387+Sui-nyan@users.noreply.github.com>
2024-09-02 22:04:44 +0200

    writing some content

--- src/app/components/ProjectCards/projectData.ts
+++ src/app/components/ProjectCards/projectData.ts
@@ -1,7 +1,7 @@
 const projectData = [
     {
         title: "Portfolio Website",
-        description: "This is what you are looking at. Teaching myself a little TypeScript and learning about Next.js",
+        description: "This is what you are looking at. A personal website to introduce myself as a Software-Developer. Teaching myself about Next.js",
         link: "https://github.com/Sui-nyan/porfolio-project",
         skills: [
             {
@@ -23,9 +23,9 @@ const projectData = [
         ]
     },
     {
-        title: "Portfolio Website",
-        description: "This",
-        link: "https://github.com/Sui-nyan/porfolio-project",
+        title: "Geekslist.de",
+        description: "A website for bookmarking movies and shows using the MovieDB API. A group project, where I was responsible for the backend part.",
+        link: "https://geekslist.de/Identity/Account/Login?ReturnUrl=%2F",
         skills: [
             {
                 name: "Java",
@@ -35,11 +35,15 @@ const projectData = [
                 name: "SpringBoot",
                 icon: "../../favicon.ico",
             },
+            {
+                name: "Docker",
+                icon: "../../favicon.ico",
+            },
         ]
     },
     {
-        title: "Portfolio Website",
-        description: "This",
+        title: "untitled plant game",
+        description: "A game project about plant care born from a game art session with a friend of mine.",
         link: "https://github.com/Sui-nyan/porfolio-project",
         skills: [
             {
@@ -53,20 +57,83 @@ const projectData = [
         ]
     },
     {
-        title: "Portfolio Website",
-        description: "This",
+        title: "Console Minesweeper",
+        description: "Small learning project made during the Advanced Programming C++ lecture.",
         link: "https://github.com/Sui-nyan/porfolio-project",
         skills: [
             {
-                name: "TailwindCss",
-                icon: "../../favicon.ico",
-            },
-            {
-                name: "TailwindCss",
+                name: "C++",
                 icon: "../../favicon.ico",
             },
         ]
-
+    },
+    {
+        title: "Rain Simulator",
+        description: "WebGL project assignment for the computer graphics course I took.",
+        link: "https://github.com/Sui-nyan/porfolio-project",
+        skills: [
+            {
+                name: "HTML/CSS",
+                icon: "../../favicon.ico",
+            },
+            {
+                name: "WebGL",
+                icon: "../../favicon.ico",
+            },
+            {
+                name: "GLSL",
+                icon: "../../favicon.ico",
+            },
+        ]
+    },
+    {
+        title: "ChadGPT",
+        description: "An iOS-App using the OpenAI-API to chat with your favourite celebrity or movie character",
+        link: "https://github.com/Sui-nyan/porfolio-project",
+        skills: [
+            {
+                name: "Swift",
+                icon: "../../favicon.ico",
+            },
+            {
+                name: "XCode",
+                icon: "../../favicon.ico",
+            },
+        ]
+    },
+    {
+        title: "greenhouse",
+        description: "A group project made during the \"Cloud Computing and Internet of Things\" lecture to get to know about cloud systems.",
+        link: "https://github.com/Sui-nyan/porfolio-project",
+        skills: [
+            {
+                name: "AWS",
+                icon: "../../favicon.ico",
+            },
+            {
+                name: "Python",
+                icon: "../../favicon.ico",
+            },
+            {
+                name: "IoT",
+                icon: "../../favicon.ico",
+            },
+        ]
+    },
+    {
+        title: "Chicken Dinner",
+        description: "A game in Unity 3D to showcase what I learned during the \"Unity 3D Development\" lecture.",
+        link: "https://github.com/Sui-nyan/porfolio-project",
+        skills: [
+            {
+                name: "Unity 3D",
+                icon: "../../favicon.ico",
+            },
+            {
+                name: "C#",
+                icon: "../../favicon.ico",
+            },
+        ]
     },
 ]
 
--- src/app/components/timeline/timelineData.ts
+++ src/app/components/timeline/timelineData.ts
@@ -10,14 +10,19 @@ const timelineData = [
         description: 'Semester abroad in China as part of the Digital Media and Packaging-Print Faculty',
     },
     {
+        date: 'February 2020 - August 2020',
+        title: 'Technical University, Vienna',
+        description: 'Started majoring in technical chemistry, but cancelled due to Covid-19',
+    },
+    {
         date: 'September 2018 - September 2019',
         title: 'International Spring College, Singapore',
-        description: 'Advanced Certificate in Chinese, while doing an Au Pair in Singapore',
+        description: 'Language school for the advanced certificate in Chinese',
     },
     {
         date: 'September 2010 - July 2018',
         title: 'Christophorus Gymnasium, Altensteig',
-        description: "Completing the German Abitur",
+        description: 'Completing the German Abitur (High-school Graduation and College Entrance Exam)',
     }
 ]
 
