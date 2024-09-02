2edf6d47b8e914d512428a5d0552aedfb4bd835e
Zhang Elisa <78491387+Sui-nyan@users.noreply.github.com>
2024-09-02 22:03:55 +0200

    ProjectCards add mobile responsiveness and title

--- src/app/components/ProjectCards/ProjectCardsContainer.tsx
+++ src/app/components/ProjectCards/ProjectCardsContainer.tsx
@@ -4,22 +4,28 @@ import ProjectSkill from "@/app/components/ProjectCards/ProjectSkill";
 
 const ProjectCardsContainer = () => {
     return (
-        <div className={styles.cardContainer}>
-            {projectData.map((data, key) =>
-                <div className={styles.card} key={key}>
-                    <a className="relative flex justify-end" href={data.link} target="_blank">
-                        <img className="h-4 w-4" src="../../Github-Logo.png" alt="Visit the project on GitHub"/>
-                    </a>
+        <div>
+            <h2 className="text-xl font-bold">Projects that I have worked on</h2>
 
-                    <h3 className="text-xl pb-1 font-semibold">{data.title}</h3>
-                    <p className="pb-4">{data.description}</p>
-                    <div className="flex flex-wrap gap-2 m-1">
-                        {data.skills.map((skill, i) => {
-                            return (<ProjectSkill skill={skill.name} icon={skill.icon} key={i}/>);})
-                        }
+            <div className={styles.cardContainer}>
+
+                {projectData.map((data, key) =>
+                    <div className={styles.card} key={key}>
+                        <a className="relative flex justify-end" href={data.link} target="_blank">
+                            <img className="h-4 w-4" src="../../Github-Logo.png" alt="Visit the project on GitHub"/>
+                        </a>
+
+                        <h3 className="text-xl pb-1 font-semibold">{data.title}</h3>
+                        <p className="pb-4">{data.description}</p>
+                        <div className="flex flex-wrap gap-2 m-1">
+                            {data.skills.map((skill, i) => {
+                                return (<ProjectSkill skill={skill.name} icon={skill.icon} key={i}/>);
+                            })
+                            }
+                        </div>
                     </div>
-                </div>
-            )}
+                )}
+            </div>
         </div>
     )
 };
